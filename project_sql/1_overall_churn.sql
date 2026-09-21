bf4899110cf30a0752edae05fc9863eab28ca16e
Prashant Jangra <p.jangra419@gmail.com>
2026-09-21 18:49:11 +0530

    update queries

--- project_sql/1_overall_churn.sql
+++ project_sql/1_overall_churn.sql
@@ -2,5 +2,5 @@ SELECT
     COUNT(*) as total_customers,
     SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) as churned_customers,
     SUM(CASE WHEN churn = 0 THEN 1 ELSE 0 END) as retained_customers,
-    CAST(100.0 * SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) / COUNT(*) AS DECIMAL(5, 2)) AS churned_rate
+    CAST(100.0 * SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) / COUNT(*) AS DECIMAL(5, 2)) AS churn_rate
 FROM telco_clean
--- project_sql/2_churn_by_partner.sql
+++ project_sql/2_churn_by_partner.sql
@@ -2,7 +2,7 @@ SELECT
     telecom_partner,
     COUNT(*) as total_customers,
     SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) as churned_customers,
-    CAST(100 * SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) / COUNT(*) AS DECIMAL(5, 2)) AS churned_rate
+    CAST(100 * SUM(CASE WHEN churn = 1 THEN 1 ELSE 0 END) / COUNT(*) AS DECIMAL(5, 2)) AS churn_rate
 FROM telco_clean
 GROUP BY telecom_partner
-ORDER BY churned_rate DESC
+ORDER BY churn_rate DESC
